9ad966cfa94033491801a47cb6be1bfcc794bb2f
benktesh <benktesh@yahoo.com>
2019-08-02 14:41:56 -0500

    Adds unit test

---
+++ ModernizeLegacyCodeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ModernizeLegacyCodeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ModernizeLegacyCodeTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd4e20c1-b27d-4471-94c1-d139594455ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ModernizeLegacyCodeTest/ResultControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModernizeLegacyCodeTest
+{
+    [TestClass]
+    public class ResultControllerTest
+    {
+        [TestMethod]
+        public void GetResults_1()
+        {
+
+        }
+    }
+}
